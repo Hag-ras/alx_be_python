d79db546b6bf31735ecfec0c822d21cc6def2810
Hagras <mohamed.hagras04@eng-st.cu.edu.eg>
2025-07-15 23:36:50 +0300

    .

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,7 @@
-principal, rate, time = 1000, 0.05, 3
+principal = 1000
+rate =  0.05
+time = 3
 
-interest = principal*rate*time
+interest = principal * rate * time
 
 print(f'The simple interest is: {interest}')
