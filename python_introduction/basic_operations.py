7e287f50e9ecaf1e1ebd7294518a835470bd68b5
Hagras <mohamed.hagras04@eng-st.cu.edu.eg>
2025-07-15 23:23:51 +0300

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 , number2 = 10 ,5
+
+sum = number2 + number1
+subtraction = number1 - number2
+multiplication = number2 * number1
+
+print(f'Addition of {number1} and {number2} is {sum} ')
+print(f'Subtraction of {number1} and {number2} is {subtraction} ')
+print(f'Multiplication of {number1} and {number2} is {multiplication} ')
