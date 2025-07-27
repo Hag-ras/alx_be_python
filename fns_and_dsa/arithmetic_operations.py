2797a8517ff833d63ebd55f505f50ff83006db48
Hagras <mohamed.hagras04@eng-st.cu.edu.eg>
2025-07-27 18:15:10 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
 def perform_operation(num1, num2, operation):
     match operation:
         case 'add':
-            print('The result is ', num1 + num2)
+            return 'The result is ' + num1 + num2
         case 'subtract':
-            print('The result is ', num1 - num2)
+            return 'The result is '+ num1 - num2
         case 'multiply':
-            print('The result is ', num1 * num2)
+            return 'The result is '+ num1 * num2
         case 'divide':
-            if num2==0:
-                print('Cannot divide by zero.')
+            if num2 == 0:
+                return 'Cannot divide by zero.'
             else:
-                print('The result is ', num1 / num2)
+                return 'The result is '+  num1 / num2
         case _:
-            print('Invalid Operation')
+            return 'Invalid Operation'
