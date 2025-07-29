4cd901e85b2d592917bc346b3f37b4e2edaee777
Hagras <mohamed.hagras04@eng-st.cu.edu.eg>
2025-07-29 10:40:08 +0300

    Added BankAccount Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount():
+    def __init__(self, balance=0):
+        self.account_balance = balance
+    def deposit(self, amount):
+        self.account_balance += amount
+        return f"Deposited: {amount}"
+    def withdraw(self, amount):
+        result = self.account_balance - amount
+        if result >= 0 :
+            self.account_balance = result
+            return f"Withdrew: {amount}"
+        else:
+            return "Insufficient funds."
+    def display_balance(self):
+        return f"Current Balance: {self.account_balance}"
+        
