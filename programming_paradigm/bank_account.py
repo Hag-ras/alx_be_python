5695f1d0a680fdee5e12f541069b5ee96be8664c
Hagras <mohamed.hagras04@eng-st.cu.edu.eg>
2025-07-29 11:45:06 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,10 +7,12 @@ class BankAccount:
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
-            return f"Withdrew: ${amount}"
+            return f"Withdrew: ${amount:.2f}"
         else:
             return "Insufficient funds."
 
+
     def display_balance(self):
-        return f"Current Balance: {self.account_balance}"
+        return f"Current Balance: ${self.account_balance:.2f}"
+
         
